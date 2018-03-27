0f0327886265067616161b7999d9cb5ff316124e
apinrdw <apinrdw@gmail.com>
2018-03-27 16:38:21 +0700

    import 2nd lib

--- app/src/main/java/com/example/apinrdw/sampleproject/MainActivity.java
+++ app/src/main/java/com/example/apinrdw/sampleproject/MainActivity.java
@@ -1,10 +1,8 @@
 package com.example.apinrdw.sampleproject;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-
-import com.example.mylibrary.HelloWorld;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +11,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        HelloWorld helloWorld = new HelloWorld();
+        com.example.mylibrary.HelloWorld helloWorld = new com.example.mylibrary.HelloWorld();
         Log.d("MainActivity", helloWorld.sayHello());
+
+        com.example.testlibrary.HelloWorld helloWorld2 = new com.example.testlibrary.HelloWorld();
+        Log.d("MainActivity", helloWorld2.sayHello());
     }
 }
